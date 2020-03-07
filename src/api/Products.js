c0d97dca4d04a53854d0d0e195cb19ad65a3cc13
Abdelrahman Tarek <abdelrahman.tarek1910@gmail.com>
2020-03-07 03:45:44 +0200

    Completed the productAPI

--- src/api/Products.js
+++ src/api/Products.js
@@ -1,11 +1,22 @@
-import Products from "./products.json";
+import Products from './products.json';
+const axios = require('axios').default;
 
-export function getAll() {
-  return Promise.resolve(Products);
+export async function getAll() {
+  try {
+    const res = await axios.get('http://127.0.0.1:2700/api/v1/products');
+    return res;
+  } catch (err) {
+    return Promise.resolve(Products);
+  }
 }
-export function getById(id) {
-  const Product = Products.find(item => item.id === id);
-  return Promise.resolve(Product);
+export async function getById(id) {
+  try {
+    const res = await axios.get(`http://127.0.0.1:2700/api/v1/products/${id}`);
+    return res;
+  } catch (err) {
+    const Product = Products.find((item) => item.id === id);
+    return Promise.resolve(Product);
+  }
 }
 
 export default {
