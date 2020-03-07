356ca3812e8474783d8cd1ea2b8b1388b9bb6ed9
Abdelrahman Tarek <abdelrahman.tarek1910@gmail.com>
2020-03-07 02:54:35 +0200

    Added product model

---
+++ src/api/models/product.model.js
@@ -0,0 +1,34 @@
+const mognoose = require('mongoose');
+
+
+const productSchema = mognoose.Schema({
+  id: {
+    type: Number,
+    unique: [true, 'This id is already taken!'],
+    required: [true, 'Please enter product id!'],
+    min: [0, 'Please enter positive id!']
+  },
+  name: {
+    type: String,
+    trim: true,
+    minlength: [5, 'The name is very short!']
+  },
+  price: {
+    type: Number,
+    required: [true, 'Please enter product price!'],
+    min: [0, 'Please enter positive price!']
+  },
+  image: {
+    type: String,
+    default: 'default.jpg'
+  },
+  description: {
+    type: String,
+    required: [true, 'Please enter product description!'],
+    minlength: [5, 'Product description is very short!']
+  }
+});
+
+const Product = mognoose.model('Product', productSchema);
+
+module.exports = Product;
